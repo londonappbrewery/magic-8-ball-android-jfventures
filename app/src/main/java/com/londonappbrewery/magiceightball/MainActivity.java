4d391112350d07d73bde7faa8ef17f8c8876b5e6
Jason <jfventures@hotmail.com>
2017-08-15 10:37:19 +0800

    I can't seem to launch my app. There seems to be a part of the code gone wrong.
    Could you help me to check?
    
    Thanks,
    Jason

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,14 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+import static android.R.attr.button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       final ImageView balldisplay;
+           balldisplay = (ImageView) findViewById(R.id.image_eightball);
+
+
+
+       final int[] ballarray= {R.drawable.ball1,
+                R.drawable.ball2,
+                             R.drawable.ball3,
+                                 R.drawable.ball4,
+                                    R.drawable.ball5};
+
+        Button myButton;
+        myButton = (Button) findViewById(R.id.askbutton);
+
+                myButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        Random randomNumberGenerator = new Random();
+
+                        int number = randomNumberGenerator.nextInt (4);
+
+                                balldisplay.setImageResource(ballarray[4]);
+
+                    }
+                });
     }
 }
+
+
+
+
